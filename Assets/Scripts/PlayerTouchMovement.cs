ce406924e2559a43a0646498d97ca0bb876f5398
BernatRosello <63212761+BernatRosello@users.noreply.github.com>
2024-09-25 23:51:50 +0200

    Joystick Virtual (Izquierdo)
    Falta generalizar para poner tambien el joystick derecho

---
+++ Assets/Scripts/PlayerTouchMovement.cs
@@ -0,0 +1,161 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem.EnhancedTouch;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.OnScreen;
+using ETouch = UnityEngine.InputSystem.EnhancedTouch;
+
+public class PlayerTouchMovement : OnScreenControl
+{
+    [SerializeField]
+    private Vector2 JoystickSize = new Vector2(300, 300);
+    [SerializeField]
+    private FloatingJoystick Joystick;
+    
+    [InputControl(layout = "Vector2")]
+    [SerializeField] private string m_ControlPath;
+    protected override string controlPathInternal
+    {
+        get => m_ControlPath;
+        set => m_ControlPath = value;
+    }
+
+    private Finger MovementFinger;
+    private Vector2 MovementAmount;
+
+
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+
+        EnhancedTouchSupport.Enable();
+        ETouch.Touch.onFingerDown += HandleFingerDown;
+        ETouch.Touch.onFingerUp += HandleLoseFinger;
+        ETouch.Touch.onFingerMove += HandleFingerMove;
+    }
+
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+
+        ETouch.Touch.onFingerDown -= HandleFingerDown;
+        ETouch.Touch.onFingerUp -= HandleLoseFinger;
+        ETouch.Touch.onFingerMove -= HandleFingerMove;
+        EnhancedTouchSupport.Disable();
+    }
+
+    private void HandleFingerMove(Finger MovedFinger)
+    {
+        if (MovedFinger == MovementFinger)
+        {
+            Vector2 knobPosition;
+            float maxMovement = JoystickSize.x / 2f;
+            ETouch.Touch currentTouch = MovedFinger.currentTouch;
+
+            if (Vector2.Distance(
+                    currentTouch.screenPosition,
+                    Joystick.RectTransform.anchoredPosition
+                ) > maxMovement)
+            {
+                knobPosition = (
+                    currentTouch.screenPosition - Joystick.RectTransform.anchoredPosition
+                    ).normalized
+                    * maxMovement;
+            }
+            else
+            {
+                knobPosition = currentTouch.screenPosition - Joystick.RectTransform.anchoredPosition;
+            }
+
+            Joystick.Knob.anchoredPosition = knobPosition;
+            MovementAmount = knobPosition / maxMovement;
+            SendValueToControl(MovementAmount);
+        }
+    }
+
+    private void HandleLoseFinger(Finger LostFinger)
+    {
+        if (LostFinger == MovementFinger)
+        {
+            MovementFinger = null;
+            Joystick.Knob.anchoredPosition = Vector2.zero;
+            Joystick.gameObject.SetActive(false);
+            MovementAmount = Vector2.zero;
+        }
+        SendValueToControl(Vector2.zero);
+    }
+
+    private void HandleFingerDown(Finger TouchedFinger)
+    {
+        if (MovementFinger == null && TouchedFinger.screenPosition.x <= Screen.width / 2f)
+        {
+            MovementFinger = TouchedFinger;
+            MovementAmount = Vector2.zero;
+            Joystick.gameObject.SetActive(true);
+            Joystick.RectTransform.sizeDelta = JoystickSize;
+            Joystick.RectTransform.anchoredPosition = ClampStartPosition(TouchedFinger.screenPosition);
+        }
+    }
+
+    private Vector2 ClampStartPosition(Vector2 StartPosition)
+    {
+        if (StartPosition.x < JoystickSize.x / 2)
+        {
+            StartPosition.x = JoystickSize.x / 2;
+        }
+
+        if (StartPosition.y < JoystickSize.y / 2)
+        {
+            StartPosition.y = JoystickSize.y / 2;
+        }
+        else if (StartPosition.y > Screen.height - JoystickSize.y / 2)
+        {
+            StartPosition.y = Screen.height - JoystickSize.y / 2;
+        }
+
+        return StartPosition;
+    }
+
+    private void OnGUI()
+    {
+        GUIStyle labelStyle = new GUIStyle()
+        {
+            fontSize = 24,
+            normal = new GUIStyleState()
+            {
+                textColor = Color.white
+            }
+        };
+        if (MovementFinger != null)
+        {
+            GUI.Label(new Rect(10, 35, 500, 20), $"Finger Start Position: {MovementFinger.currentTouch.startScreenPosition}", labelStyle);
+            GUI.Label(new Rect(10, 65, 500, 20), $"Finger Current Position: {MovementFinger.currentTouch.screenPosition}", labelStyle);
+        }
+        else
+        {
+            GUI.Label(new Rect(10, 35, 500, 20), "No Current Movement Touch", labelStyle);
+        }
+
+        GUI.Label(new Rect(10, 10, 500, 20), $"Screen Size ({Screen.width}, {Screen.height})", labelStyle);
+    }
+
+    /* #if UNITY_EDITOR
+        [CustomEditor(typeof(PlayerTouchMovement))]
+        internal class PlayerTouchMovementEditor : UnityEditor.Editor
+        {
+            private SerializedProperty m_ControlPathInternal;
+            public void OnEnable()
+            {
+                m_ControlPathInternal = serializedObject.FindProperty(nameof(PlayerTouchMovement.m_ControlPath));
+            }
+            public override void OnInspectorGUI()
+            {
+                EditorGUILayout.PropertyField(m_ControlPathInternal);
+
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+    #endif */
+}
