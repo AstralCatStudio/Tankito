fe6d04831895fdd5d803af0e43437d72f6d89e84
m4nu3lgc <loloandthebeastcactus@gmail.com>
2024-10-13 02:35:03 +0200

    BasicMenu with SmoothTransition

---
+++ Assets/Scripts/Menus/MenuController.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MenuController : Singleton<MenuController>
+{
+    [Serializable]
+    public enum menuName
+    {
+        InitialScreenLogIn = 0,
+        MainMenu = 1,
+        Settings = 2,
+        Credits = 3,
+        LogOut = 4,
+        Shop = 5,
+        PlayMenu = 6,
+        Lobby = 7,
+        CharacterSelection = 8
+    }
+    public GameObject[] menus;
+    public GameObject[] bgMenus;
+    public Vector2[] bgPositions; //Posiciones del "grid"
+    
+    public int currentMenuIndex;
+    public int xUnit = 56, yUnit = 30;  
+    public float bgParallaxFactor = 1.2f, mgParallaxFactor = 1f, fgParallaxFactor = 0.7f;
+
+    public UnityEvent<Vector2,GameObject,GameObject> menuChanged;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BgReposition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeToMenu(int newMenuIndex)
+    {
+        menus[(int)currentMenuIndex].gameObject.SetActive(false);
+        MoveBG(newMenuIndex);
+        currentMenuIndex = newMenuIndex;
+        menus[(int)currentMenuIndex].gameObject.SetActive(true);
+    }
+
+    private void MoveBG(int newMenuIndex)
+    {
+        Transform bg;
+        Transform mg;
+        Transform fg;
+        Vector2 newTranslation = bgPositions[(int)currentMenuIndex] - bgPositions[(int)newMenuIndex];
+
+        menuChanged?.Invoke(newTranslation, bgMenus[(int)currentMenuIndex], bgMenus[(int)newMenuIndex]);
+
+        for (int i = 0; i < bgMenus.Length; i++)
+        {
+            if(i == currentMenuIndex || i == newMenuIndex)
+            {
+                continue;
+            }
+            bg = bgMenus[i].transform.GetChild(0);
+            mg = bgMenus[i].transform.GetChild(1);
+            fg = bgMenus[i].transform.GetChild(2);
+
+            bg.Translate(xUnit * bgParallaxFactor * newTranslation.x, yUnit * bgParallaxFactor * newTranslation.y, 0);
+            mg.Translate(xUnit * mgParallaxFactor * newTranslation.x, yUnit * mgParallaxFactor * newTranslation.y, 0);
+            fg.Translate(xUnit * fgParallaxFactor * newTranslation.x, yUnit * fgParallaxFactor * newTranslation.y, 0);
+        }
+    }
+
+    public void CheckPasswordAndUsername()
+    {
+        //Hacer comprobaciones del nombre y la contraseña
+    }
+
+    public void EnterAsGuest()
+    {
+        //Esto activa que el jugador está como invitado
+    }
+
+    private void BgReposition()
+    {
+        Transform bg;
+        Transform mg;
+        Transform fg;
+        for(int i=0; i < bgMenus.Length; i++)
+        {
+            bg = bgMenus[i].transform.GetChild(0);
+            mg = bgMenus[i].transform.GetChild(1);
+            fg = bgMenus[i].transform.GetChild(2);
+
+            bg.Translate(xUnit * bgParallaxFactor * bgPositions[i].x, yUnit * bgParallaxFactor * bgPositions[i].y, 0);
+            mg.Translate(xUnit * mgParallaxFactor * bgPositions[i].x, yUnit * mgParallaxFactor * bgPositions[i].y, 0);
+            fg.Translate(xUnit * fgParallaxFactor * bgPositions[i].x, yUnit * fgParallaxFactor * bgPositions[i].y, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Menus/SmoothTransition.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.TerrainTools;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SmoothTransition : MonoBehaviour
+{
+    [SerializeField] float lerpDuration = 3;
+    float timeElapsed;
+    Vector2 newTranslation;
+    MenuController menu;
+    Transform currentBg, currentMg, currentFg;
+    Transform newBg, newMg, newFg;
+    bool isTransitioning;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu = MenuController.Instance;
+        timeElapsed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isTransitioning)
+        {
+            MoveSmoothly();
+        }
+    }
+
+    public void MoveEvent(Vector2 newTranslation, GameObject currentBgMenu, GameObject newBgMenu)
+    {
+        this.newTranslation = newTranslation;
+
+        currentBg = currentBgMenu.transform.GetChild(0);
+        currentMg = currentBgMenu.transform.GetChild(1);
+        currentFg = currentBgMenu.transform.GetChild(2);
+
+        newBg = newBgMenu.transform.GetChild(0);
+        newMg = newBgMenu.transform.GetChild(1);
+        newFg = newBgMenu.transform.GetChild(2);
+
+        Debug.Log(currentBgMenu);
+        Debug.Log(newBgMenu);
+
+        isTransitioning = true;
+        timeElapsed = 0;
+    }
+
+    private void MoveSmoothly()
+    {
+        if (timeElapsed < lerpDuration)
+        {
+            float t = timeElapsed;
+
+            Vector2[] bg = new Vector2[2];
+            Vector2[] mg = new Vector2[2];
+            Vector2[] fg = new Vector2[2];
+
+            bg[0] = (currentBg.transform.position); bg[1] = (newBg.transform.position);
+            mg[0] = (currentMg.transform.position); mg[1] = (newMg.transform.position);
+            fg[0] = (currentFg.transform.position); fg[1] = (newFg.transform.position);
+
+            Vector2 bgCurrent = new Vector2(menu.xUnit * menu.bgParallaxFactor * newTranslation.x, menu.yUnit * menu.bgParallaxFactor * newTranslation.y);
+            Vector2 mgCurrent = new Vector2(menu.xUnit * menu.mgParallaxFactor * newTranslation.x, menu.yUnit * menu.mgParallaxFactor * newTranslation.y);
+            Vector2 fgCurrent = new Vector2(menu.xUnit * menu.fgParallaxFactor * newTranslation.x, menu.yUnit * menu.fgParallaxFactor * newTranslation.y);
+
+            bg[0] = Vector2.Lerp(bg[0], bgCurrent, t / lerpDuration);
+            mg[0] = Vector2.Lerp(mg[0], mgCurrent, t / lerpDuration);
+            fg[0] = Vector2.Lerp(fg[0], fgCurrent, t / lerpDuration);
+            bg[1] = Vector2.Lerp(bg[1], Vector2.zero, t / lerpDuration);
+            mg[1] = Vector2.Lerp(mg[1], Vector2.zero, t / lerpDuration);
+            fg[1] = Vector2.Lerp(fg[1], Vector2.zero, t / lerpDuration);
+
+            currentBg.transform.position = bg[0];
+            currentMg.transform.position = mg[0];
+            currentFg.transform.position = fg[0];
+            newBg.transform.position = bg[1];
+            newMg.transform.position = mg[1];
+            newFg.transform.position = fg[1];
+
+            timeElapsed += Time.deltaTime;
+        } else
+        {
+            isTransitioning = false;
+            timeElapsed = 0;
+        }
+    }
+}
