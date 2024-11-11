b483e6d22dc60d90d1c1764712429a1f42ef1d37
leunamanuelgc <loloandthebeastcactus@gmail.com>
2024-11-11 18:29:15 +0100

    me rindo y a lo mejor lo hago la animacion de las algas con el sprite skinning

--- Assets/Scripts/Menus/AlgaeAnimation.cs
+++ Assets/Scripts/Menus/AlgaeAnimation.cs
@@ -18,8 +18,8 @@ public class AlgaeAnimation : MonoBehaviour
     {
         container = GetComponent<SplineContainer>();
         random = Random.Range(0, Mathf.PI);
-        defaultHeadRot = head.localRotation;
-        defaultTailRot = tail.localRotation;
+        //defaultHeadRot = head.localRotation;
+        //defaultTailRot = tail.localRotation;
     }
 
     // Update is called once per frame
@@ -29,7 +29,6 @@ public class AlgaeAnimation : MonoBehaviour
         int knotCount = spline.Count;
         BezierKnot knot;
         
-
         for (int i = knotCount-1; i > 0; i--)
         {
             float distanceDamping = (float)i / knotCount;
@@ -37,26 +36,27 @@ public class AlgaeAnimation : MonoBehaviour
             knot.Position.x = Mathf.Sin(i * (Mathf.PI / 2) + Time.time * speed + random) * distanceDamping * maxWidthOffset;
             spline[i] = knot;
         }
-        knot = spline[0];
-        Quaternion knotRotation = new(
-            knot.Rotation.value.x,
-            knot.Rotation.value.y,
-            knot.Rotation.value.z,
-            knot.Rotation.value.w
-        );
 
-        tail.transform.localRotation = Quaternion.Euler(knotRotation.eulerAngles + defaultTailRot.eulerAngles);
-        tail.transform.localPosition = new Vector3(knot.Position.x, knot.Position.y, knot.Position.z);
+        //knot = spline[0];
+        //Quaternion knotRotation = new(
+        //    knot.Rotation.value.x,
+        //    knot.Rotation.value.y,
+        //    knot.Rotation.value.z,
+        //    knot.Rotation.value.w
+        //);
 
-        knot = container.Spline[^1];    //^1 quiere decir que toma el valor 1 desde el final
-        knotRotation = new(
-            knot.Rotation.value.x,
-            knot.Rotation.value.y,
-            knot.Rotation.value.z,
-            knot.Rotation.value.w
-        );
+        //tail.transform.localRotation = Quaternion.Euler(knotRotation.eulerAngles + defaultTailRot.eulerAngles);
+        //tail.transform.localPosition = new Vector3(knot.Position.x, knot.Position.y, knot.Position.z);
 
-        head.transform.localRotation = Quaternion.Euler(knotRotation.eulerAngles - defaultHeadRot.eulerAngles);
-        head.transform.localPosition = new Vector3(knot.Position.x, knot.Position.y, knot.Position.z);
+        //knot = container.Spline[^1];    //^1 quiere decir que toma el valor 1 desde el final
+        //knotRotation = new(
+        //    knot.Rotation.value.x,
+        //    knot.Rotation.value.y,
+        //    knot.Rotation.value.z,
+        //    knot.Rotation.value.w
+        //);
+
+        //head.transform.localRotation = Quaternion.Euler(knotRotation.eulerAngles - defaultHeadRot.eulerAngles);
+        //head.transform.localPosition = new Vector3(knot.Position.x, knot.Position.y, knot.Position.z);
     }
 }
