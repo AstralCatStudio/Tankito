49025ce0f55ce51da1e95f0b00ff64fa22419c85
antoonrs <arodriguezseselle@gmail.com>
2024-11-18 20:46:35 +0100

    BodyGuardNPC_Final commit

--- Assets/Scripts/Comportamiento de personajes/BodyGuard/BodyGuardScript.cs
+++ Assets/Scripts/Comportamiento de personajes/BodyGuard/BodyGuardScript.cs
@@ -1,17 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BehaviourAPI.Core;
 
-public class BodyGuardScript : MonoBehaviour
+public class BodyGuardBehaviours : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
