7c3fb85d026d9eadd64baafaa0a51c5235e1dbd8
m4nu3lgc <loloandthebeastcactus@gmail.com>
2024-11-19 02:45:57 +0100

    Anuncio de Weefager

--- Assets/Scripts/Menus/DropdownMoney.cs
+++ Assets/Scripts/Menus/DropdownMoney.cs
@@ -2,23 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class DropdownMoney : MonoBehaviour
 {
+    [SerializeField] private GameObject ad;
+    [SerializeField] private VideoPlayer videoPlayer;
+    private Coroutine videoCoroutine;
+
     public void PurchaseVirtualMoney(int moneyAmount)
     {
         ClientData clientData = ClientData.Instance;
         clientData.ChangeMoney(+moneyAmount);
 
-        MusicManager.Instance.PlaySoundPitch("snd_monedas",0.2f);
+        MusicManager.Instance.PlaySoundPitch("snd_monedas", 0.2f);
     }
     public void WatchAd()
     {
-        int moneyAmount = Random.Range(1, 3);
-        ClientData clientData = ClientData.Instance;
-        clientData.ChangeMoney(+moneyAmount);
+        ad.SetActive(true);
+
+        //Hacer que la musica pare y no sea interactuable el resto del menú
+        videoCoroutine = StartCoroutine(EndAd());
 
         MusicManager.Instance.PlaySound("cancelar");
     }
+
+    public void StopAd()
+    {
+        StopCoroutine(videoCoroutine);
+        ad.SetActive(false);
+    }
+
+    IEnumerator EndAd()
+    {
+        float i = 0;
+        while (i < videoPlayer.clip.length)
+        {
+            i += Time.deltaTime;
+            yield return null;
+        }
+        ad.SetActive(false);
+        int moneyAmount = Random.Range(1, 3);
+        ClientData.Instance.ChangeMoney(+moneyAmount);
+        MusicManager.Instance.PlaySoundPitch("snd_monedas", 0.2f);
+    }
+
 }
