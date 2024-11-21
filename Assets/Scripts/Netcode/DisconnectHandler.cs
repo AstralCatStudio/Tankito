01354b1eb38f59d5bbee1ed85d2d7ec6c76bb31b
albertoalegreb <albertoalegre28@gmail.com>
2024-11-21 19:23:55 +0100

    Intento de arreglo de la desconexión

--- Assets/Scripts/Netcode/DisconnectHandler.cs
+++ Assets/Scripts/Netcode/DisconnectHandler.cs
@@ -25,13 +25,51 @@ public class DisconnectHandler : NetworkBehaviour
     {
         if (IsServer)
         {
+            DespawnAllClients();
+            
             DisconnectAllClientRpc();
 
             Invoke(nameof(Disconnect), 0.7f);
         }
         else
         {
+            DespawnClientServerRpc(NetworkManager.Singleton.LocalClientId);
+
             Disconnect();
+        }
+    }
+
+    private void DespawnAllClients()
+    {
+        foreach (var client in NetworkManager.Singleton.ConnectedClientsList)
+        {
+            if (client.PlayerObject != null && client.PlayerObject.TryGetComponent<NetworkObject>(out var networkObject))
+            {
+                networkObject.Despawn();
+            }
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void DespawnClientServerRpc(ulong clientId)
+    {
+        if (NetworkManager.Singleton.ConnectedClients.TryGetValue(clientId, out var client))
+        {
+            var playerObject = client.PlayerObject;
+
+            if (playerObject != null)
+            {
+                playerObject.Despawn(true);
+                Debug.LogWarning($"Jugador con ClientID {clientId} ha sido despaweado.");
+            }
+            else
+            {
+                Debug.LogWarning($"No se encontró un PlayerObject para el ClientID {clientId}.");
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"El ClientID {clientId} no está conectado.");
         }
     }
 
