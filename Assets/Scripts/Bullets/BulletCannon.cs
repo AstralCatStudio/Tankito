e128b3babe8b2668b555f408e408cebb796298c2
lucjuego <lucjuega@gmail.com>
2024-11-23 03:00:17 +0100

    balas multiples hechas casi

--- Assets/Scripts/Bullets/BulletCannon.cs
+++ Assets/Scripts/Bullets/BulletCannon.cs
@@ -32,10 +32,12 @@ namespace Tankito
         float interval = 1;
         float timer = 0;
         List<GameObject> bulletsShot = new List<GameObject>();
-        private List<Vector2> BulletDirection = new List<Vector2>();
+        private List<Vector2> BulletDirections = new List<Vector2>();
         public int baseBulletAmount;
-        int bulletAmount;
+        public int m_bulletAmount;
         int spawnTickTime = 0;
+        [SerializeField]
+        float m_shootRadius, m_shootSpreadAngle, m_scatterAngle;
         //public Queue<GameObject> simulatedBullets = new Queue<GameObject>();
         public List<BulletModifier> Modifiers { get => m_bulletModifiers; }
         public BulletProperties Properties { get => m_bulletProperties; }
@@ -58,9 +60,9 @@ namespace Tankito
 
         public void ApplyModifierProperties(int nRound = 0)
         {
-            BulletDirection.Clear();
-            BulletDirection.Add(transform.right);
-            bulletAmount = baseBulletAmount;
+            BulletDirections.Clear();
+            BulletDirections.Add(new Vector2(0,1)) ;
+            m_bulletAmount = baseBulletAmount;
             m_bulletProperties = BulletCannonRegistry.Instance.BaseProperties;
             foreach (BulletModifier modifier in m_bulletModifiers)
             {
@@ -69,41 +71,41 @@ namespace Tankito
                 m_bulletProperties.acceleration += modifier.bulletStatsModifier.accelerationAdded;
                 m_bulletProperties.bouncesTotal += modifier.bulletStatsModifier.BouncesAdded;
                 m_bulletProperties.lifetimeTotal += modifier.bulletStatsModifier.lifeTimeAdded;
-                BulletDirection.AddRange(modifier.bulletStatsModifier.BulletDirections);
-                bulletAmount += modifier.bulletStatsModifier.amountAdded;
-                bulletAmount *= modifier.bulletStatsModifier.amountMultiplier;
+                BulletDirections.AddRange(modifier.bulletStatsModifier.BulletDirections);
+                m_bulletAmount += modifier.bulletStatsModifier.amountAdded;
+                m_bulletAmount *= modifier.bulletStatsModifier.amountMultiplier;
             }
         }
 
         public void Shoot(Vector2 aimVector)
         {
-                if (timer >= interval)
+            if (timer >= interval)
+            {
+                timer = 0;
+                float baseAngle = Mathf.Atan2(aimVector.y, aimVector.x) ;
+                for (int i = 0; i < BulletDirections.Count; i++)
                 {
-                    timer = 0;
-                    ShootBullet(, aimVector);
+                    float newAngle = Mathf.Atan2(BulletDirections[i].y, BulletDirections[i].x)+ baseAngle;
+                    for (int j = 0; j < m_bulletAmount; j++)
+                    {
+                        float angle = newAngle - (m_shootSpreadAngle / 2 + (m_shootSpreadAngle / (m_bulletAmount + 1)) * (j+1))*Mathf.Deg2Rad;
+                        Debug.Log("bala "+(j+1) + ": angulo " + angle);
+                        Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+                        ShootBullet(m_shootRadius* direction, direction, j*i);
+                    }
                 }
+            }
         }
 
         void ShootBullet(Vector2 position, Vector2 direction, int spawnN)
         {
-            //Vector2 direction;
-            //float angle;
-            //foreach (var item in BulletDirection)
-            //{
-            //    for (int i = 0; i < bulletAmount; i++)
-            //    {
-            //        if (bulletAmount % 2 == 0)
-            //        {
-
-            //        }
-            //    }
-            //}
+            
             m_bulletProperties.direction = direction;
             m_bulletProperties.startingPosition = transform.position;
             m_bulletProperties.spawnTickTime = SimClock.TickCounter;
-            
-            var newBullet = BulletPool.Instance.Get(position, direction, OwnerClientId, SimClock.TickCounter, spawnN);
-            newBullet.AddToSim();
+            Instantiate<GameObject>(BulletCannonRegistry.Instance.m_bulletPrefab);
+            //var newBullet = BulletPool.Instance.Get(position, direction, OwnerClientId, SimClock.TickCounter, spawnN);
+            //newBullet.AddToSim();
         }
 
         void Update()
--- Assets/Scripts/Bullets/BulletController.cs
+++ Assets/Scripts/Bullets/BulletController.cs
@@ -20,6 +20,7 @@ namespace Tankito {
         public float m_lifetime = 0; // Life Time counter
         protected Vector2 lastCollisionNormal = Vector2.zero;
         private Rigidbody2D m_rb;
+        public float selfCollisionTreshold = 0.1f;
         public Action<BulletController> OnSpawn = (ABullet) => { }, OnFly = (ABullet) => { }, OnHit = (ABullet) => { }, OnBounce = (ABullet) => { }, OnDetonate = (ABullet) => { };
         private void Awake()
         {
@@ -27,7 +28,8 @@ namespace Tankito {
         }
         
         private void OnEnable()
-        {           
+        {
+            
             GetComponent<BulletSimulationObject>().OnComputeKinematics += MoveBullet;
 
             transform.position = BulletCannonRegistry.Instance[ownerId].transform.position;
@@ -53,13 +55,17 @@ namespace Tankito {
         private void Update()
         {
             transform.rotation = Quaternion.LookRotation(new Vector3(0, 0, 1), m_rb.velocity.normalized);
+            MoveBullet(Time.deltaTime);//borrar
         }
 
         void MoveBullet(float deltaTime)
         {
-            
+            if(m_lifetime >= selfCollisionTreshold)
+            {
+                gameObject.layer = 0;
+            }
             m_rb.velocity += (BulletCannonRegistry.Instance[ownerId].Properties.acceleration != 0f) ? BulletCannonRegistry.Instance[ownerId].Properties.acceleration * m_rb.velocity.normalized : Vector2.zero;
-            m_lifetime += Time.deltaTime;
+            m_lifetime += deltaTime;
             OnFly.Invoke(this);
             if (m_lifetime >= BulletCannonRegistry.Instance[ownerId].Properties.lifetimeTotal)
             {
@@ -85,7 +91,8 @@ namespace Tankito {
             if (NetworkManager.Singleton.IsServer)
             {
                 BulletSimulationObject bulletSimulation = GetComponent<BulletSimulationObject>();
-                BulletPool.Instance.Release(bulletSimulation);
+                Destroy(gameObject);
+                //BulletPool.Instance.Release(bulletSimulation);
             }
         }   
 
