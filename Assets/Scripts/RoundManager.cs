d1e2c6ac77a6ad24f140ce3c224cde7c957bd64c
BernatRosello <63212761+BernatRosello@users.noreply.github.com>
2024-11-24 23:37:06 +0100

    Update RoundManager.cs

--- Assets/Scripts/RoundManager.cs
+++ Assets/Scripts/RoundManager.cs
@@ -154,9 +154,9 @@ namespace Tankito
 
         private void TankDeath(TankData t)
         {
-            Debug.Log("Round Manager registered a tank death");
             t.AwardPoints(m_players.Count - AliveTanks.Count() - 1); // -1 porque no deberia darte puntos por estar tu mismo muerto
             PlayerListUpdate(true);
+            Debug.Log($"Round Manager registered a tank death: Tank[{t.OwnerClientId}]-Points: {t.Points}");
         }
 
         private void PlayerListUpdate(bool updateInputs = false)
