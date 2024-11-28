1653b6b56884ba47990b8a05b3f96a1eeae695a9
albertoalegreb <albertoalegre28@gmail.com>
2024-11-28 17:27:08 +0100

    Intento arreglo copiar codigo auto

--- Assets/Scripts/Utils/CopyToClipboard.cs
+++ Assets/Scripts/Utils/CopyToClipboard.cs
@@ -3,20 +3,13 @@ using UnityEngine;
 
 public class CopyToClipboard : MonoBehaviour
 {
-    // Función de JavaScript que copia al portapapeles
-    [DllImport("__Internal")]
-    private static extern void passCopyToBrowser(string text);
-
     public void CopyTextToClipboard(string text)
     {
 #if UNITY_WEBGL && !UNITY_EDITOR
-        passCopyToBrowser(text);  // Llama a la función JavaScript para copiar el texto
+        GUIUtility.systemCopyBuffer = text;
+        WebGLCopyAndPasteAPI.passCopyToBrowser(text);
 #else
-        // En plataformas que no sean WebGL, usa el enfoque estándar
-        TextEditor te = new TextEditor();
-        te.text = text;
-        te.SelectAll();
-        te.Copy();
+        GUIUtility.systemCopyBuffer = text;
 #endif
     }
 }
