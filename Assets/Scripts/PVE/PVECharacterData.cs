71cd4bf9ffc7f6fbd737d27f88b7195a110bb1f8
albertoalegreb <albertoalegre28@gmail.com>
2024-12-04 23:57:50 +0100

    Scripts para manejar personajes vivos
    Se ha creado un PVECharacterData para manejar si un personaje esta vivo y cuanta vida tiene. Para los enemigos esta PVEEnemyData que implementa la anterior y toma la vida maxima del scriptableObject que ahora tiene un campo de vida

---
+++ Assets/Scripts/PVE/PVECharacterData.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tankito.SinglePlayer
+{
+    public class PVECharacterData : MonoBehaviour
+    {
+        [SerializeField] protected int m_maxHealth = 2;
+        [SerializeField] protected int m_health;
+        [SerializeField] protected bool m_isAlive;
+
+        public int Health => m_health;
+        public bool Alive => m_isAlive;
+
+        protected virtual void Start()
+        {
+            m_health = m_maxHealth;
+            m_isAlive = true;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            m_health -= damage;
+
+            if (m_health <= 0) Die();
+        }
+
+        public void AddHealth(int addedHealth)
+        {
+            m_health = (m_health + addedHealth > m_maxHealth) ? m_maxHealth : m_health + addedHealth;
+        }
+
+        protected virtual void Die()
+        {
+            Debug.Log($"El personaje {gameObject.name} fue derrotado");
+            m_isAlive = false;
+            gameObject.SetActive(false);
+
+        }
+
+        public void Revive()
+        {
+            Debug.Log($"El personaje {gameObject.name} fue derrotado");
+            m_health = m_maxHealth;
+            m_isAlive = true;
+            gameObject.SetActive(true);
+        }
+
+        public void Revive(int newHealth)
+        {
+            Debug.Log($"El personaje {gameObject.name} fue derrotado");
+            m_health = newHealth;
+            m_isAlive = true;
+            gameObject.SetActive(true);
+        }
+    }
+}
+
---
+++ Assets/Scripts/PVE/PVEEnemyData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tankito.SinglePlayer
+{
+    public class PVEEnemyData : PVECharacterData
+    {
+        protected override void Start()
+        {
+            m_maxHealth = GetComponent<AgentController>().npcData.health;
+            base.Start();
+        }
+
+        protected override void Die()
+        {
+            base.Die();
+            Debug.Log("Se crea resto para revivir con el necromancer");
+            // Instaciar prefab de resto
+        }
+    }
+}
+
