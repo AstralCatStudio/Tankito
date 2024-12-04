c617e9713882cb8e96e02452a8c839c322283737
albertoalegreb <albertoalegre28@gmail.com>
2024-12-04 22:21:18 +0100

    Pantalla final con generación dinámica
    Ahora los elementos del menú se generan por código

--- Assets/Scripts/AnimatedUI/WinMenu.cs
+++ Assets/Scripts/AnimatedUI/WinMenu.cs
@@ -1,84 +1,98 @@
 using System.Collections;
+using NUnit.Framework;
+using UnityEditor;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class WinMenu : MonoBehaviour
 {
     public int m_num_players = 0;
     public int m_max_players = 4;
 
-    public GameObject marcadorJugador1;
-    public GameObject marcadorJugador2;
-    public GameObject marcadorJugador3;
-    public GameObject marcadorJugador4;
+    [SerializeField] private GameObject prefabSprite;
+    [SerializeField] private GameObject prefabMarcador;
+    [SerializeField] private GameObject prefabPuesto;
 
-    public GameObject spriteJugador1;
-    public GameObject spriteJugador2;
-    public GameObject spriteJugador3;
-    public GameObject spriteJugador4;
+    [SerializeField] private GameObject spritesParent;
+    [SerializeField] private GameObject marcadoresParent;
+    [SerializeField] private GameObject puestosParent;
 
-    public GameObject puesto1;
-    public GameObject puesto2;
-    public GameObject puesto3;
-    public GameObject puesto4;
+    [SerializeField] private List<GameObject> spritesList;
+    [SerializeField] private List<GameObject> marcadoresList;
+    [SerializeField] private List<GameObject> puestosList;
 
-    public GameObject exitButton;
-
-    public float offsetY = 50f;
+    public float offsetY;
 
     public float animDuration = 0.5f;
     public float scaleDuration = 0.3f;
 
+    private void Awake()
+    {
+        spritesList = new List<GameObject>();
+        marcadoresList = new List<GameObject>();
+        puestosList = new List<GameObject>();
+    }
+
     void Start()
     {
+        InitWinMenu(m_num_players);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Inicio animacion");
+            StartCoroutine(AnimateSprites());
+        }
     }
 
     private void OnEnable()
     {
-        StartCoroutine(AnimateSprites());
+        //StartCoroutine(AnimateSprites());
+    }
+
+    public void InitWinMenu(int playersCount)
+    {
+        for (int i = 0; i < playersCount; i++)
+        {
+            GameObject newSprite = Instantiate(prefabSprite, spritesParent.transform);
+            newSprite.transform.GetChild(0).gameObject.SetActive(false);
+            spritesList.Add(newSprite);
+            GameObject newMarcador = Instantiate(prefabMarcador, marcadoresParent.transform);
+            newMarcador.transform.GetChild(0).gameObject.SetActive(false);
+            marcadoresList.Add(newMarcador);
+            GameObject newPuesto = Instantiate(prefabPuesto, puestosParent.transform);
+            newPuesto.transform.GetChild(0).gameObject.SetActive(false);
+            puestosList.Add(newPuesto);
+        }
     }
 
     IEnumerator AnimateSprites()
     {
-        GameObject[] sprites = { spriteJugador4, spriteJugador3, spriteJugador2, spriteJugador1 };
-        GameObject[] marcadores = { marcadorJugador4, marcadorJugador3, marcadorJugador2, marcadorJugador1 };
-        GameObject[] puestos = { puesto4, puesto3, puesto2, puesto1 };
-        string[] soundNames = { "WinTankitoSounds1", "WinTankitoSounds2", "WinTankitoSounds3", "WinTankitoSounds4" };
+        string[] soundNames = { "WinTankitoSounds4", "WinTankitoSounds3", "WinTankitoSounds2", "WinTankitoSounds1" };
 
-        for (int i = m_max_players - m_num_players; i < m_max_players; i++)
+        for (int i = m_num_players - 1; i >= 0; i--)
         {
-            GameObject sprite = sprites[i];
-            GameObject marcador = marcadores[i];
-            GameObject puesto = puestos[i];
+            RectTransform sprite = spritesList[i].transform.GetChild(0).GetComponent<RectTransform>();
+            RectTransform marcador = marcadoresList[i].transform.GetChild(0).GetComponent<RectTransform>();
+            RectTransform puesto = puestosList[i].transform.GetChild(0).GetComponent<RectTransform>();
+
+            sprite.gameObject.SetActive(true);
+            LeanTween.moveY(sprite, offsetY, animDuration).setEase(LeanTweenType.easeOutBounce);
 
 
-            RectTransform marcadorTransform = marcador.GetComponent<RectTransform>();
-            RectTransform spriteTransform = sprite.GetComponent<RectTransform>();
-            
-            float targetY = offsetY;
-
-            sprite.SetActive(true);
-            
-            LeanTween.moveY(spriteTransform, targetY, animDuration).setEase(LeanTweenType.easeOutBounce);
-
-            puesto.SetActive(true);
-            RectTransform puestoTransform = puesto.GetComponent<RectTransform>();
-            puestoTransform.localScale = Vector3.zero;
-            LeanTween.scale(puestoTransform, Vector3.one, scaleDuration).setEase(LeanTweenType.easeOutBack);
+            puesto.localScale = Vector3.zero;
+            puesto.gameObject.SetActive(true);
+            LeanTween.scale(puesto, Vector3.one, scaleDuration).setEase(LeanTweenType.easeOutBack);
 
             MusicManager.Instance.PlaySound(soundNames[i]);
 
             yield return new WaitForSeconds(animDuration);
 
-            marcador.SetActive(true);
-            RectTransform marcadorRectTransform = marcador.GetComponent<RectTransform>();
-            marcadorRectTransform.localScale = Vector3.zero;
-            LeanTween.scale(marcadorRectTransform, Vector3.one, scaleDuration).setEase(LeanTweenType.easeOutBack);
+            marcador.localScale = Vector3.zero;
+            marcador.gameObject.SetActive(true);
+            LeanTween.scale(marcador, Vector3.one, scaleDuration).setEase(LeanTweenType.easeOutBack);
         }
-
-
-        exitButton.SetActive(true);
-        RectTransform exitButtonTransform = exitButton.GetComponent<RectTransform>();
-        exitButtonTransform.localScale = Vector3.zero;
-        LeanTween.scale(exitButtonTransform, Vector3.one, scaleDuration).setEase(LeanTweenType.easeOutBack);
     }
 }
