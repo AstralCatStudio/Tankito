37d4e104b172f07459f6db7969c5c010c6ff1186
m4nu3lgc <loloandthebeastcactus@gmail.com>
2024-12-06 19:34:21 +0100

    anton te pusheo la cara

---
+++ Assets/Scripts/PVE/Leftovers.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using Tankito.SinglePlayer;
+using UnityEngine;
+
+public class Leftovers : MonoBehaviour
+{
+    public tankType type;
+}
