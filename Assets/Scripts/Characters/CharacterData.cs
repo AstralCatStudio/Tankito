471276439d4bc41594f85dec6ed55f99e9905caf
antoonrs <arodriguezseselle@gmail.com>
2024-12-11 21:33:57 +0100

    Reapply "animaciones de peces"
    This reverts commit f7335996c894f3145abcb1d578d470445054b441.

--- Assets/Scripts/Characters/CharacterData.cs
+++ Assets/Scripts/Characters/CharacterData.cs
@@ -10,7 +10,8 @@ public class CharacterData : ScriptableObject
     public Sprite tankHead;
     public Sprite tankBody;
     public Sprite characterIcon;
-    public Animation[] animations;
+    public Sprite fishSprite;
+    public RuntimeAnimatorController fishAnimator;
     public string characterName;
     [TextArea(5,10)]
     public string description;
